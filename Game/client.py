a3bf837499fb160785d738363d2543ff72e8f0b2
gunmar06 <49071084+gunmar06@users.noreply.github.com>
2019-05-10 12:54:17 +0200

    Update client.py

--- Game/client.py
+++ Game/client.py
@@ -1,1 +1,37 @@
-﻿#Client side
+from tkinter import *
+from tkinter.font import Font
+from time import sleep
+import socket
+import select
+import threading
+
+class MenuPrincipal(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self.InitInterface()
+        self.mainloop()
+    def InitInterface(self):
+        self.geometry("900x900")
+        self.configure(bg="black")
+        self.title("Demineur en cooperation")
+        self.resizable(width=FALSE, height=FALSE)
+        self.MainInterface()
+    def MainInterface(self):
+        self.interface=Canvas(self,width=900,height=900,bg='grey',bd=0)
+        self.font=Font(family="Helvetica",size=14)
+        self.interface.pack()
+        solo=Button(self.interface, text="jouer en solo",bg='#999999',width=50,height=4, font=self.font)
+        solo.place(x=170,y=350)
+        multi=Button(self.interface, text="jouer en multi-joueurs",bg='#999999',width=50,height=4, font=self.font)
+        multi.place(x=170,y=500)
+        quitter=Button(self.interface, text="quitter",bg='#999999',width=4,height=2, font=self.font)
+        quitter.place(x=850,y=850)
+
+class Main(MenuPrincipal):
+    def __init__(self):
+        MenuPrincipal.__init__(self)
+
+
+        
+
+main=Main()
