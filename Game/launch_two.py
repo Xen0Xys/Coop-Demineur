dce0e063b3ced36c6338a7680f0afcf148205d9d
Xen0Xys <47594493+Xen0Xys@users.noreply.github.com>
2019-05-15 14:42:17 +0200

    Update launch_two.py

--- Game/launch_two.py
+++ Game/launch_two.py
@@ -1,6 +1,18 @@
-from client import Main as client
-from server import Main as server
+from server import Main as Server
+from tkinter.font import Font
+from random import randint
+from tkinter import *
 import threading
+import socket
+import select
+from client import Main as client
+from time import sleep
 
-threading.Thread(target=server).start()
-client()
+def StartClient1():
+    main1 = client()
+
+def StartClient2():
+    main2 = client()
+
+threading.Thread(target=StartClient1).start()
+StartClient2()
